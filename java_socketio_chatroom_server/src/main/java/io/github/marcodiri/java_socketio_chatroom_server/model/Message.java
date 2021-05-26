7e97d9e9dca8b2c95f1606f993cdcb6b70dac538
marcodiri <marcodiri96@gmail.com>
2021-05-26 13:23:22 +0200

    Overridden Objects methods in Message

--- java_socketio_chatroom_server/src/main/java/io/github/marcodiri/java_socketio_chatroom_server/model/Message.java
+++ java_socketio_chatroom_server/src/main/java/io/github/marcodiri/java_socketio_chatroom_server/model/Message.java
@@ -1,6 +1,7 @@
 package io.github.marcodiri.java_socketio_chatroom_server.model;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public final class Message {
 	private final Timestamp timestamp;
@@ -24,4 +25,29 @@ public final class Message {
 	public String getMessage() {
 		return message;
 	}
+	
+	@Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Message{");
+        sb.append("timestamp=").append(timestamp);
+        sb.append(", user=").append(user);
+        sb.append(", message=").append(message);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Message msg = (Message) o;
+        return Objects.equals(timestamp, msg.timestamp) && Objects.equals(user, msg.user) && Objects.equals(message, msg.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timestamp, user, message);
+    }
 }
