8ace770888a87e3dcdba2170e533a068f511cdd3
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-05-28 15:26:07 +0200

    Fix chatroomName

--- java_socketio_chatroom_server/src/main/java/io/github/marcodiri/java_socketio_chatroom_server/ChatroomServer.java
+++ java_socketio_chatroom_server/src/main/java/io/github/marcodiri/java_socketio_chatroom_server/ChatroomServer.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class ChatroomServer {
 
-    private final String chatroomName = "chatroom";
+    private final String chatroomName = "Chatroom";
 
     private final ServerWrapper serverWrapper;
 
