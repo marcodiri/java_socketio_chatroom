a8f1a0676928b4493aa77b9293cca72006d57975
marcodiri <marcodiri96@gmail.com>
2021-07-08 11:51:34 +0200

    removed url from pom and code formatting

--- java_socketio_chatroom_server/src/test/java/io/github/marcodiri/java_socketio_chatroom_server/ChatroomServerTest.java
+++ java_socketio_chatroom_server/src/test/java/io/github/marcodiri/java_socketio_chatroom_server/ChatroomServerTest.java
@@ -24,136 +24,142 @@ import static org.awaitility.Awaitility.await;
 
 public class ChatroomServerTest {
 
-    private Socket clientSocket;
+	private Socket clientSocket;
 
-    @Mock
-    private ServerRepository serverRepository;
+	@Mock
+	private ServerRepository serverRepository;
 
-    @InjectMocks
-    private ChatroomServer chatroomServer;
+	@InjectMocks
+	private ChatroomServer chatroomServer;
 
-    private AutoCloseable closeable;
+	private AutoCloseable closeable;
 
-    @Before
-    public void setup() {
-        closeable = MockitoAnnotations.openMocks(this);
-        try {
-            chatroomServer.start();
-        } catch (Exception ignored) {
-            fail("ServerWrapper startup failed");
-        }
+	@Before
+	public void setup() {
+		closeable = MockitoAnnotations.openMocks(this);
+		try {
+			chatroomServer.start();
+		} catch (Exception ignored) {
+			fail("ServerWrapper startup failed");
+		}
 
-        clientSocket = IO.socket(URI.create("http://localhost:3000"), IO.Options.builder().build());
-    }
+		clientSocket = IO.socket(URI.create("http://localhost:3000"), IO.Options.builder().build());
+	}
 
-    @After
-    public void closeConnectionsAndReleaseMocks() throws Exception {
-        clientSocket.disconnect();
-        chatroomServer.stop();
-        closeable.close();
-    }
+	@After
+	public void closeConnectionsAndReleaseMocks() throws Exception {
+		clientSocket.disconnect();
+		chatroomServer.stop();
+		closeable.close();
+	}
 
-    @Test
-    public void testClientJoinRetrievesMessagesFromDb() {
-        List<Message> history = new ArrayList<>();
-        Message msg1 = new Message(new Timestamp(System.currentTimeMillis()), "user1", "message1");
-        Message msg2 = new Message(new Timestamp(System.currentTimeMillis()), "user2", "message2");
-        history.add(msg1);
-        history.add(msg2);
-        when(serverRepository.findAll()).thenReturn(history);
+	@Test
+	public void testClientJoinRetrievesMessagesFromDb() {
+		List<Message> history = new ArrayList<>();
+		Message msg1 = new Message(new Timestamp(System.currentTimeMillis()), "user1", "message1");
+		Message msg2 = new Message(new Timestamp(System.currentTimeMillis()), "user2", "message2");
+		history.add(msg1);
+		history.add(msg2);
+		when(serverRepository.findAll()).thenReturn(history);
 
-        List<Message> retrievedMessages = new ArrayList<>();
-        clientSocket.on("msg", arg -> {
-            JSONObject jsonMsg = (JSONObject) arg[0];
-            Message incomingMessage = new Message(new Timestamp(jsonMsg.getLong("timestamp")), jsonMsg.getString("user"), jsonMsg.getString("message"));
-            retrievedMessages.add(incomingMessage);
-        });
-        clientSocket.on(Socket.EVENT_CONNECT, objects -> clientSocket.emit("join"));
-        clientSocket.connect();
+		List<Message> retrievedMessages = new ArrayList<>();
+		clientSocket.on("msg", arg -> {
+			JSONObject jsonMsg = (JSONObject) arg[0];
+			Message incomingMessage = new Message(new Timestamp(jsonMsg.getLong("timestamp")),
+					jsonMsg.getString("user"), jsonMsg.getString("message"));
+			retrievedMessages.add(incomingMessage);
+		});
+		clientSocket.on(Socket.EVENT_CONNECT, objects -> clientSocket.emit("join"));
+		clientSocket.connect();
 
-        try {
-            await().atMost(2, SECONDS).until(() -> retrievedMessages.containsAll(asList(msg1, msg2)));
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-            fail("Expected " + asList(msg1, msg2) + " but got " + retrievedMessages);
-        }
-        verify(serverRepository, times(1)).findAll();
-    }
+		try {
+			await().atMost(2, SECONDS).until(() -> retrievedMessages.containsAll(asList(msg1, msg2)));
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Expected " + asList(msg1, msg2) + " but got " + retrievedMessages);
+		}
+		verify(serverRepository, times(1)).findAll();
+	}
 
-    @Test
-    public void testClientReceivesItsMessages() {
-        Message originalMessage1 = new Message(new Timestamp(System.currentTimeMillis()), "user1", "message1");
-        Message originalMessage2 = new Message(new Timestamp(System.currentTimeMillis()), "user2", "message2");
+	@Test
+	public void testClientReceivesItsMessages() {
+		Message originalMessage1 = new Message(new Timestamp(System.currentTimeMillis()), "user1", "message1");
+		Message originalMessage2 = new Message(new Timestamp(System.currentTimeMillis()), "user2", "message2");
 
-        when(serverRepository.findAll()).thenReturn(new ArrayList<>());
+		when(serverRepository.findAll()).thenReturn(new ArrayList<>());
 
-        List<Message> retrievedMessages = new ArrayList<>();
-        clientSocket.on("msg", arg -> {
-            JSONObject jsonMsg = (JSONObject) arg[0];
-            Message incomingMessage = new Message(new Timestamp(jsonMsg.getLong("timestamp")), jsonMsg.getString("user"), jsonMsg.getString("message"));
-            retrievedMessages.add(incomingMessage);
-        });
-        clientSocket.on(Socket.EVENT_CONNECT, objects -> {
-            clientSocket.emit("join");
-            clientSocket.emit("msg", originalMessage1.toJSON());
-            clientSocket.emit("msg", originalMessage2.toJSON());
-        });
-        clientSocket.connect();
+		List<Message> retrievedMessages = new ArrayList<>();
+		clientSocket.on("msg", arg -> {
+			JSONObject jsonMsg = (JSONObject) arg[0];
+			Message incomingMessage = new Message(new Timestamp(jsonMsg.getLong("timestamp")),
+					jsonMsg.getString("user"), jsonMsg.getString("message"));
+			retrievedMessages.add(incomingMessage);
+		});
+		clientSocket.on(Socket.EVENT_CONNECT, objects -> {
+			clientSocket.emit("join");
+			clientSocket.emit("msg", originalMessage1.toJSON());
+			clientSocket.emit("msg", originalMessage2.toJSON());
+		});
+		clientSocket.connect();
 
-        try {
-            await().atMost(2, SECONDS).until(() -> retrievedMessages.containsAll(asList(originalMessage1, originalMessage2)));
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-            fail("Expected " + asList(originalMessage1, originalMessage2) + " but got " + retrievedMessages);
-        }
-    }
+		try {
+			await().atMost(2, SECONDS)
+					.until(() -> retrievedMessages.containsAll(asList(originalMessage1, originalMessage2)));
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Expected " + asList(originalMessage1, originalMessage2) + " but got " + retrievedMessages);
+		}
+	}
 
-    @Test
-    public void testMessagesAreSavedInDb() {
-        Message originalMessage1 = new Message(new Timestamp(System.currentTimeMillis()), "user1", "message1");
-        Message originalMessage2 = new Message(new Timestamp(System.currentTimeMillis()), "user2", "message2");
+	@Test
+	public void testMessagesAreSavedInDb() {
+		Message originalMessage1 = new Message(new Timestamp(System.currentTimeMillis()), "user1", "message1");
+		Message originalMessage2 = new Message(new Timestamp(System.currentTimeMillis()), "user2", "message2");
 
-        when(serverRepository.findAll()).thenReturn(new ArrayList<>());
+		when(serverRepository.findAll()).thenReturn(new ArrayList<>());
 
-        List<Message> retrievedMessages = new ArrayList<>();
-        clientSocket.on("msg", arg -> {
-            JSONObject jsonMsg = (JSONObject) arg[0];
-            Message incomingMessage = new Message(new Timestamp(jsonMsg.getLong("timestamp")), jsonMsg.getString("user"), jsonMsg.getString("message"));
-            retrievedMessages.add(incomingMessage);
-        });
-        clientSocket.on(Socket.EVENT_CONNECT, objects -> {
-            clientSocket.emit("join");
-            clientSocket.emit("msg", originalMessage1.toJSON());
-            clientSocket.emit("msg", originalMessage2.toJSON());
-        });
-        clientSocket.connect();
+		List<Message> retrievedMessages = new ArrayList<>();
+		clientSocket.on("msg", arg -> {
+			JSONObject jsonMsg = (JSONObject) arg[0];
+			Message incomingMessage = new Message(new Timestamp(jsonMsg.getLong("timestamp")),
+					jsonMsg.getString("user"), jsonMsg.getString("message"));
+			retrievedMessages.add(incomingMessage);
+		});
+		clientSocket.on(Socket.EVENT_CONNECT, objects -> {
+			clientSocket.emit("join");
+			clientSocket.emit("msg", originalMessage1.toJSON());
+			clientSocket.emit("msg", originalMessage2.toJSON());
+		});
+		clientSocket.connect();
 
-        ArgumentCaptor<Message> argumentCaptor = ArgumentCaptor.forClass(Message.class);
-        try {
-            await().atMost(2, SECONDS).until(() -> !retrievedMessages.isEmpty());
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-            fail("Expected " + asList(originalMessage1, originalMessage2) + " but got " + retrievedMessages);
-        }
-        verify(serverRepository, times(2)).save(argumentCaptor.capture());
-        List<Message> capturedArgument = argumentCaptor.getAllValues();
+		ArgumentCaptor<Message> argumentCaptor = ArgumentCaptor.forClass(Message.class);
+		try {
+			await().atMost(2, SECONDS).until(() -> !retrievedMessages.isEmpty());
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Expected " + asList(originalMessage1, originalMessage2) + " but got " + retrievedMessages);
+		}
+		verify(serverRepository, times(2)).save(argumentCaptor.capture());
+		List<Message> capturedArgument = argumentCaptor.getAllValues();
 
-        assertThat(capturedArgument).contains(originalMessage1, originalMessage2);
-    }
+		assertThat(capturedArgument).contains(originalMessage1, originalMessage2);
+	}
 
-    @Test
-    public void testRoomSizeWhenClientJoinsAndWhenLeaves() {
-        clientSocket.on(Socket.EVENT_CONNECT, objects -> clientSocket.emit("join"));
-        clientSocket.connect();
+	@Test
+	public void testRoomSizeWhenClientJoinsAndWhenLeaves() {
+		clientSocket.on(Socket.EVENT_CONNECT, objects -> clientSocket.emit("join"));
+		clientSocket.connect();
 
-        try {
-            await().atMost(2, SECONDS).until(() -> chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length == 1);
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-            fail("Expected 1 but got " + chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length);
-        }
-        clientSocket.emit("leave");
-        try {
-            await().atMost(2, SECONDS).until(() -> chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length == 0);
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-            fail("Expected 0 but got " + chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length);
-        }
+		try {
+			await().atMost(2, SECONDS)
+					.until(() -> chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length == 1);
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Expected 1 but got " + chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length);
+		}
+		clientSocket.emit("leave");
+		try {
+			await().atMost(2, SECONDS)
+					.until(() -> chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length == 0);
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Expected 0 but got " + chatroomServer.getNamespace().getAdapter().listClients("Chatroom").length);
+		}
 
-    }
+	}
 }
