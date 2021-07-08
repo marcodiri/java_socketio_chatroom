8a7df567011b83bbd6e1a1f857890de191616072
marcodiri <marcodiri96@gmail.com>
2021-07-08 15:32:53 +0200

    added handleMessage method and View interface

--- java_socketio_chatroom_client/src/main/java/io/github/marcodiri/java_socketio_chatroom_client/ChatroomClient.java
+++ java_socketio_chatroom_client/src/main/java/io/github/marcodiri/java_socketio_chatroom_client/ChatroomClient.java
@@ -1,7 +1,10 @@
 package io.github.marcodiri.java_socketio_chatroom_client;
 
 import java.net.URI;
+import org.json.JSONObject;
+
 import io.github.marcodiri.java_socketio_chatroom_client.model.Message;
+import io.github.marcodiri.java_socketio_chatroom_client.view.ClientView;
 import io.socket.client.IO;
 import io.socket.client.IO.Options;
 import io.socket.client.Socket;
@@ -9,9 +12,12 @@ import io.socket.client.Socket;
 public class ChatroomClient {
 
 	private final Socket socket;
+	
+	private ClientView view;
 
-	public ChatroomClient(URI uri, Options options) {
+	public ChatroomClient(URI uri, Options options, ClientView view) {
 		this.socket = IO.socket(uri, options);
+		this.view = view;
 	}
 
 	public Socket getSocket() {
@@ -25,6 +31,7 @@ public class ChatroomClient {
 	public void connect() {
 		socket.on(Socket.EVENT_CONNECT, objects -> {
 			socket.emit("join");
+			handleMessage();
 		});
 		socket.connect();
 	}
@@ -39,6 +46,10 @@ public class ChatroomClient {
 		} else {
 			throw new RuntimeException("Unable to send message when not connected to server");
 		}
+	}
+
+	private void handleMessage() {
+		socket.on("msg",arg -> view.addMessage(new Message((JSONObject) arg[0])));
 	}
 
 }
--- java_socketio_chatroom_client/src/test/java/io/github/marcodiri/java_socketio_chatroom_client/ChatroomClientTest.java
+++ java_socketio_chatroom_client/src/test/java/io/github/marcodiri/java_socketio_chatroom_client/ChatroomClientTest.java
@@ -1,9 +1,12 @@
 package io.github.marcodiri.java_socketio_chatroom_client;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.fail;
 import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.net.URI;
 import java.sql.Timestamp;
@@ -13,36 +16,38 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import io.github.marcodiri.java_socketio_chatroom_client.model.Message;
+import io.github.marcodiri.java_socketio_chatroom_client.view.ClientView;
 import io.github.marcodiri.java_socketio_chatroom_server_mock.ChatroomServerMock;
 import io.socket.client.IO;
 
 public class ChatroomClientTest {
-	
+
+	private ClientView view;
+
 	private ChatroomClient client;
-	
+
 	private ChatroomServerMock serverMock;
-	
+
 	@Before
 	public void setup() {
-		client = new ChatroomClient(
-				URI.create("http://localhost:3000"),
-				IO.Options.builder().build()
-				);
+		view = mock(ClientView.class);
+		client = new ChatroomClient(URI.create("http://localhost:3000"), IO.Options.builder().build(), view);
+
 		serverMock = new ChatroomServerMock();
 		try {
-            serverMock.start();
-        } catch (Exception ignored) {
-            fail("Server startup failed");
-        }
+			serverMock.start();
+		} catch (Exception ignored) {
+			fail("Server startup failed");
+		}
 	}
-	
+
 	@After
 	public void closeConnections() {
-		client.getSocket().disconnect();
+		client.disconnect();
 		try {
 			serverMock.stop();
 		} catch (Exception e) {
-            fail("Failed to stop the server");
+			fail("Failed to stop the server");
 		}
 	}
 
@@ -50,21 +55,22 @@ public class ChatroomClientTest {
 	public void testConnect() {
 		client.connect();
 		try {
-            await().atMost(2, SECONDS).untilTrue(serverMock.socketIsInRoom());
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-            fail("Socket could not join the room");
-        }
+			await().atMost(2, SECONDS).untilTrue(serverMock.socketIsInRoom());
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Socket could not join the room");
+		}
+
+		assertThat(client.getSocket().hasListeners("msg")).isTrue();
 	}
-	
+
 	@Test
 	public void testSendMessageWhenClientNotConnected() {
 		Message msg = new Message(new Timestamp(System.currentTimeMillis()), "user", "message");
-		
-		assertThatThrownBy(() -> client.sendMessage(msg))
-		.isInstanceOf(RuntimeException.class)
-		.hasMessage("Unable to send message when not connected to server");
+
+		assertThatThrownBy(() -> client.sendMessage(msg)).isInstanceOf(RuntimeException.class)
+				.hasMessage("Unable to send message when not connected to server");
 	}
-	
+
 	@Test
 	public void testSendMessageWhenClientConnected() {
 		client.connect();
@@ -73,7 +79,7 @@ public class ChatroomClientTest {
 		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
 			fail("Client could not connect to server");
 		}
-		
+
 		try {
 			serverMock.handleEvent("msg", arg -> {
 				serverMock.receivedMsg = (JSONObject) arg[0];
@@ -81,19 +87,38 @@ public class ChatroomClientTest {
 		} catch (NullPointerException e) {
 			fail("Socket is not connected to server");
 		}
-		
+
 		Message msg = new Message(new Timestamp(System.currentTimeMillis()), "user", "message");
 		try {
 			client.sendMessage(msg);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
-		
+
 		try {
-            await().atMost(2, SECONDS).until(() -> msg.equals(new Message(serverMock.receivedMsg)));
-        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
-        	fail("Server did not receive the correct message");
-        }
+			await().atMost(2, SECONDS).until(() -> msg.equals(new Message(serverMock.receivedMsg)));
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Server did not receive the correct message");
+		}
+	}
+
+	@Test
+	public void testHandleMessage() {
+		client.connect();
+		try {
+			await().atMost(2, SECONDS).until(() -> client.isConnected());
+		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
+			fail("Client could not connect to server");
+		}
+
+		Message msg = new Message(new Timestamp(System.currentTimeMillis()), "user", "message");
+		try {
+			serverMock.sendEvent("msg", msg.toJSON());
+		} catch (NullPointerException e) {
+			fail("Socket is null");
+		}
+		
+		verify(view).addMessage(msg);
 	}
 
 }
