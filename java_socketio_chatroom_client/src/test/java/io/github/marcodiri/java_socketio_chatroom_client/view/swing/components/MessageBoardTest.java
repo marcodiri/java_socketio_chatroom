cb4bb13fb591c6dc57d130135e62033a2fcd5041
marcodiri <marcodiri96@gmail.com>
2021-07-22 12:20:25 +0200

    Modified MessageBoard tests to fix compatibility with Github Actions

--- java_socketio_chatroom_client/src/test/java/io/github/marcodiri/java_socketio_chatroom_client/view/swing/components/MessageBoardTest.java
+++ java_socketio_chatroom_client/src/test/java/io/github/marcodiri/java_socketio_chatroom_client/view/swing/components/MessageBoardTest.java
@@ -2,6 +2,9 @@ package io.github.marcodiri.java_socketio_chatroom_client.view.swing.components;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,22 +22,29 @@ public class MessageBoardTest {
 
 	@Test
 	public void testNewMessageNotifyWhenBoardIsEmpty() {
-		Message msg = new ClientMessage(new Timestamp(0), "user", "message");
+		Timestamp timestamp = new Timestamp(0);
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		
+		Message msg = new ClientMessage(timestamp, "user", "message");
 		board.newMessageNotify(msg);
 		assertThat(board.getHistory()).containsExactly(msg);
-		String formattedMsg = "01:00 user: message";
+		
+		String formattedMsg = dateFormat.format(timestamp) + " user: message";
 		String x = board.getText();
 		assertThat(x).isEqualTo(formattedMsg);
 	}
 	
 	@Test
 	public void testNewMessageNotifyWhenBoardIsNotEmpty() {
+		Timestamp timestamp = new Timestamp(0);
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		
 		String previousText =  "Text";
 		board.setText(previousText);
 		Message msg = new ClientMessage(new Timestamp(0), "user", "message");
 		board.newMessageNotify(msg);
 		assertThat(board.getHistory()).containsExactly(msg);
-		String formattedMsg = "01:00 user: message";
+		String formattedMsg = dateFormat.format(timestamp) + " user: message";
 		assertThat(board.getText()).isEqualTo(previousText + System.lineSeparator() + formattedMsg);
 	}
 	
