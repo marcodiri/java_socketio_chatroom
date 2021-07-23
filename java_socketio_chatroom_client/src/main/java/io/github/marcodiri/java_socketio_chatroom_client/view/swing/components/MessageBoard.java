7a57058647159a09fc233310326b24acb8657953
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-07-23 10:58:02 +0200

    Removed unused imports

--- java_socketio_chatroom_client/src/main/java/io/github/marcodiri/java_socketio_chatroom_client/view/swing/components/MessageBoard.java
+++ java_socketio_chatroom_client/src/main/java/io/github/marcodiri/java_socketio_chatroom_client/view/swing/components/MessageBoard.java
@@ -1,14 +1,11 @@
 package io.github.marcodiri.java_socketio_chatroom_client.view.swing.components;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 import javax.swing.JTextPane;
 
-import io.github.marcodiri.java_socketio_chatroom_client.model.ClientMessage;
 import io.github.marcodiri.java_socketio_chatroom_core.model.Message;
 
 @SuppressWarnings("serial")
