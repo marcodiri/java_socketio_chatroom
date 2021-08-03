2d9d223f2bbb8520249344b0393213bf9d131be0
marcodiri <marcodiri96@gmail.com>
2021-08-03 10:33:59 +0200

    Fixed Server not closing AfterClass

--- java_socketio_chatroom_e2e/src/e2e/java/io/github/marcodiri/java_socketio_chatroom_e2e/MultipleChatroomClientE2E.java
+++ java_socketio_chatroom_e2e/src/e2e/java/io/github/marcodiri/java_socketio_chatroom_e2e/MultipleChatroomClientE2E.java
@@ -10,6 +10,7 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.github.marcodiri.java_socketio_chatroom_server.App;
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.fixture.FrameFixture;
@@ -17,6 +18,7 @@ import org.assertj.swing.fixture.JButtonFixture;
 import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,29 +39,38 @@ public class MultipleChatroomClientE2E extends AssertJSwingJUnitTestCase {
 
 	private MongoClient mongoClient;
 
+	private static Thread serverThread;
+
 	List<Frame> frames;
 
 	@BeforeClass
 	public static void setup() {
 		// start the Server
-		application("io.github.marcodiri.java_socketio_chatroom_server.App").start();
+		serverThread = new Thread(() -> App.main(null));
+		serverThread.start();
 	}
-	
+
+	@AfterClass
+	public static void stopServer() throws InterruptedException {
+		serverThread.interrupt();
+		serverThread.join();
+	}
+
 	@Override
-	protected void onSetUp() throws Exception {
+	protected void onSetUp() {
 		int mongoPort = Integer.parseInt(System.getProperty("mongo.port", "27017"));
 		mongoClient = new MongoClient(new ServerAddress("localhost", mongoPort));
 		// always start with an empty database
 		mongoClient.getDatabase(CHATROOM_DB_NAME).drop();
-		
+
 		// start the Clients
 		application("io.github.marcodiri.java_socketio_chatroom_client.App").start();
 		application("io.github.marcodiri.java_socketio_chatroom_client.App").start();
-		
+
 		// get a reference of Clients JFrame
 		try {
             await().atMost(5, SECONDS).until(() -> {
-            	frames = new ArrayList<Frame>();
+            	frames = new ArrayList<>();
             	Frame[] fs = Frame.getFrames();
             	for (Frame f : fs) {
 					if ("Socket.io Chatroom".equals(f.getTitle()) && f.isShowing()) {
@@ -85,14 +96,14 @@ public class MultipleChatroomClientE2E extends AssertJSwingJUnitTestCase {
 		FrameFixture window2 = new FrameFixture(robot(), frames.get(1));
 		window1.moveTo(new Point(0, 0));
 		window2.moveTo(new Point(500, 0));
-		
+
 		JButtonFixture btnConnect1 = window1.button(JButtonMatcher.withText("Connect"));
 		JButtonFixture btnDisconnect1 = window1.button(JButtonMatcher.withText("Disconnect"));
 		JButtonFixture btnSend1 = window1.button(JButtonMatcher.withText("Send"));
 		JTextComponentFixture txtUsername1 = window1.textBox("txtUsername");
 		JTextComponentFixture txtMessage1 = window1.textBox("txtMessage");
 		JTextComponentFixture msgsTextPane1 = window1.textBox("msgsTextPane");
-		
+
 		JButtonFixture btnConnect2 = window2.button(JButtonMatcher.withText("Connect"));
 		JButtonFixture btnDisconnect2 = window2.button(JButtonMatcher.withText("Disconnect"));
 		JButtonFixture btnSend2 = window2.button(JButtonMatcher.withText("Send"));
@@ -127,11 +138,11 @@ public class MultipleChatroomClientE2E extends AssertJSwingJUnitTestCase {
 		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
 			fail("Client1 message not displayed in message board of Client2");
 		}
-		
+
 		// Client2 sends message
 		txtMessage2.enterText(MESSAGE_2_TEXT);
 		btnSend2.click();
-		
+
 		try {
 			await().atMost(2, SECONDS).untilAsserted(() -> assertThat(msgsTextPane1.text()).contains(MESSAGE_2_USER, MESSAGE_2_TEXT));
 		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
@@ -149,27 +160,27 @@ public class MultipleChatroomClientE2E extends AssertJSwingJUnitTestCase {
 		FrameFixture window2 = new FrameFixture(robot(), frames.get(1));
 		window1.moveTo(new Point(0, 0));
 		window2.moveTo(new Point(500, 0));
-		
+
 		JButtonFixture btnConnect1 = window1.button(JButtonMatcher.withText("Connect"));
 		JButtonFixture btnDisconnect1 = window1.button(JButtonMatcher.withText("Disconnect"));
 		JTextComponentFixture txtUsername1 = window1.textBox("txtUsername");
-		
+
 		JButtonFixture btnConnect2 = window2.button(JButtonMatcher.withText("Connect"));
 		JTextComponentFixture txtUsername2 = window2.textBox("txtUsername");
         JTextComponentFixture txtErrorMessage2 = window2.textBox("txtErrorMessage");
 
 		txtUsername1.enterText(MESSAGE_1_USER);
 		btnConnect1.click();
-		
+
 		try {
 			await().atMost(2, SECONDS).untilAsserted(() -> btnDisconnect1.requireEnabled());
 		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
 			fail("Client1 message disconnect button not enabled");
 		}
-		
+
 		txtUsername2.enterText(MESSAGE_1_USER);
 		btnConnect2.click();
-		
+
 		try {
 			await().atMost(2, SECONDS).untilAsserted(() -> assertThat(txtErrorMessage2.text()).isNotEmpty());
 		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
--- java_socketio_chatroom_e2e/src/e2e/java/io/github/marcodiri/java_socketio_chatroom_e2e/SingleChatroomClientE2E.java
+++ java_socketio_chatroom_e2e/src/e2e/java/io/github/marcodiri/java_socketio_chatroom_e2e/SingleChatroomClientE2E.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.sql.Timestamp;
 import javax.swing.JFrame;
 
+import io.github.marcodiri.java_socketio_chatroom_server.App;
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.core.matcher.JButtonMatcher;
@@ -18,6 +19,7 @@ import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.bson.Document;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,107 +30,116 @@ import com.mongodb.ServerAddress;
 @RunWith(GUITestRunner.class)
 public class SingleChatroomClientE2E extends AssertJSwingJUnitTestCase {
 
-	public static final String CHATROOM_DB_NAME = "chatroom";
-	public static final String MESSAGES_COLLECTION_NAME = "messages";
+    public static final String CHATROOM_DB_NAME = "chatroom";
+    public static final String MESSAGES_COLLECTION_NAME = "messages";
 
-	private static final Timestamp MESSAGE_1_TIMESTAMP = new Timestamp(0);
-	private static final String MESSAGE_1_USER = "user1";
-	private static final String MESSAGE_1_TEXT = "text1";
-	private static final Timestamp MESSAGE_2_TIMESTAMP = new Timestamp(1000000);
-	private static final String MESSAGE_2_USER = "user2";
-	private static final String MESSAGE_2_TEXT = "text2";
+    private static final Timestamp MESSAGE_1_TIMESTAMP = new Timestamp(0);
+    private static final String MESSAGE_1_USER = "user1";
+    private static final String MESSAGE_1_TEXT = "text1";
+    private static final Timestamp MESSAGE_2_TIMESTAMP = new Timestamp(1000000);
+    private static final String MESSAGE_2_USER = "user2";
+    private static final String MESSAGE_2_TEXT = "text2";
 
-	private MongoClient mongoClient;
+    private MongoClient mongoClient;
 
-	private FrameFixture window;
+    private static Thread serverThread;
 
-	@BeforeClass
-	public static void setup() {
-		// start the Server
-		application("io.github.marcodiri.java_socketio_chatroom_server.App").start();
-	}
+    private FrameFixture window;
 
-	@Override
-	protected void onSetUp() throws Exception {
-		int mongoPort = Integer.parseInt(System.getProperty("mongo.port", "27017"));
-		mongoClient = new MongoClient(new ServerAddress("localhost", mongoPort));
-		// always start with an empty database
-		mongoClient.getDatabase(CHATROOM_DB_NAME).drop();
-		// add some messages to the database
-		addTestMessageToDatabase(MESSAGE_1_TIMESTAMP, MESSAGE_1_USER, MESSAGE_1_TEXT);
-		addTestMessageToDatabase(MESSAGE_2_TIMESTAMP, MESSAGE_2_USER, MESSAGE_2_TEXT);
-		// start the Client
-		application("io.github.marcodiri.java_socketio_chatroom_client.App").start();
-		// get a reference of Client JFrame
-		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
-			@Override
-			protected boolean isMatching(JFrame frame) {
-				return "Socket.io Chatroom".equals(frame.getTitle()) && frame.isShowing();
-			}
-		}).using(robot());
-	}
+    @BeforeClass
+    public static void setup() {
+        // start the Server
+        serverThread = new Thread(() -> App.main(null));
+        serverThread.start();
+    }
 
-	@Override
-	protected void onTearDown() {
-		mongoClient.close();
-	}
+    @AfterClass
+    public static void stopServer() throws InterruptedException {
+        serverThread.interrupt();
+        serverThread.join();
+    }
 
-	@Test
-	@GUITest
-	public void testOnConnectAllDatabaseElementsAreShown() {
-		JButtonFixture btnConnect = window.button(JButtonMatcher.withText("Connect"));
-		JButtonFixture btnDisconnect = window.button(JButtonMatcher.withText("Disconnect"));
-		JTextComponentFixture txtUsername = window.textBox("txtUsername");
-		JTextComponentFixture msgsTextPane = window.textBox("msgsTextPane");
+    @Override
+    protected void onSetUp() {
+        int mongoPort = Integer.parseInt(System.getProperty("mongo.port", "27017"));
+        mongoClient = new MongoClient(new ServerAddress("localhost", mongoPort));
+        // always start with an empty database
+        mongoClient.getDatabase(CHATROOM_DB_NAME).drop();
+        // add some messages to the database
+        addTestMessageToDatabase(MESSAGE_1_TIMESTAMP, MESSAGE_1_USER, MESSAGE_1_TEXT);
+        addTestMessageToDatabase(MESSAGE_2_TIMESTAMP, MESSAGE_2_USER, MESSAGE_2_TEXT);
+        // start the Client
+        application("io.github.marcodiri.java_socketio_chatroom_client.App").start();
+        // get a reference of Client JFrame
+        window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+            @Override
+            protected boolean isMatching(JFrame frame) {
+                return "Socket.io Chatroom".equals(frame.getTitle()) && frame.isShowing();
+            }
+        }).using(robot());
+    }
 
-		txtUsername.enterText("User");
-		btnConnect.click();
+    @Override
+    protected void onTearDown() {
+        mongoClient.close();
+    }
 
-		try {
-			await().atMost(2, SECONDS).untilAsserted(() -> assertThat(msgsTextPane.text()).contains(MESSAGE_1_USER,
-					MESSAGE_1_TEXT, MESSAGE_2_USER, MESSAGE_2_TEXT));
-		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
-			fail("Saved message not displayed in message board");
-		}
-		
-		btnDisconnect.click();
-	}
+    @Test
+    @GUITest
+    public void testOnConnectAllDatabaseElementsAreShown() {
+        JButtonFixture btnConnect = window.button(JButtonMatcher.withText("Connect"));
+        JButtonFixture btnDisconnect = window.button(JButtonMatcher.withText("Disconnect"));
+        JTextComponentFixture txtUsername = window.textBox("txtUsername");
+        JTextComponentFixture msgsTextPane = window.textBox("msgsTextPane");
 
-	@Test
-	@GUITest
-	public void testSendButtonSuccess() {
-		JButtonFixture btnConnect = window.button(JButtonMatcher.withText("Connect"));
-		JButtonFixture btnDisconnect = window.button(JButtonMatcher.withText("Disconnect"));
-		JButtonFixture btnSend = window.button(JButtonMatcher.withText("Send"));
-		JTextComponentFixture txtUsername = window.textBox("txtUsername");
-		JTextComponentFixture txtMessage = window.textBox("txtMessage");
-		JTextComponentFixture msgsTextPane = window.textBox("msgsTextPane");
+        txtUsername.enterText("User");
+        btnConnect.click();
+
+        try {
+            await().atMost(2, SECONDS).untilAsserted(() -> assertThat(msgsTextPane.text()).contains(MESSAGE_1_USER,
+                    MESSAGE_1_TEXT, MESSAGE_2_USER, MESSAGE_2_TEXT));
+        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
+            fail("Saved message not displayed in message board");
+        }
+
+        btnDisconnect.click();
+    }
+
+    @Test
+    @GUITest
+    public void testSendButtonSuccess() {
+        JButtonFixture btnConnect = window.button(JButtonMatcher.withText("Connect"));
+        JButtonFixture btnDisconnect = window.button(JButtonMatcher.withText("Disconnect"));
+        JButtonFixture btnSend = window.button(JButtonMatcher.withText("Send"));
+        JTextComponentFixture txtUsername = window.textBox("txtUsername");
+        JTextComponentFixture txtMessage = window.textBox("txtMessage");
+        JTextComponentFixture msgsTextPane = window.textBox("msgsTextPane");
 
 
-		txtUsername.enterText(MESSAGE_1_USER);
-		btnConnect.click();
+        txtUsername.enterText(MESSAGE_1_USER);
+        btnConnect.click();
 
-		try {
-			await().atMost(2, SECONDS).untilAsserted(() -> txtMessage.requireEnabled());
-		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
-			fail("Message text box not enabled");
-		}
+        try {
+            await().atMost(2, SECONDS).untilAsserted(() -> txtMessage.requireEnabled());
+        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
+            fail("Message text box not enabled");
+        }
 
-		txtMessage.enterText(MESSAGE_1_TEXT);
-		btnSend.click();
+        txtMessage.enterText(MESSAGE_1_TEXT);
+        btnSend.click();
 
-		try {
-			await().atMost(2, SECONDS).untilAsserted(() -> assertThat(msgsTextPane.text()).contains(MESSAGE_1_USER, MESSAGE_1_TEXT));
-		} catch (org.awaitility.core.ConditionTimeoutException ignored) {
-			fail("Sent message not displayed in message board");
-		}
+        try {
+            await().atMost(2, SECONDS).untilAsserted(() -> assertThat(msgsTextPane.text()).contains(MESSAGE_1_USER, MESSAGE_1_TEXT));
+        } catch (org.awaitility.core.ConditionTimeoutException ignored) {
+            fail("Sent message not displayed in message board");
+        }
 
-		btnDisconnect.click();
-	}
+        btnDisconnect.click();
+    }
 
-	private void addTestMessageToDatabase(Timestamp timestamp, String user, String message) {
-		mongoClient.getDatabase(CHATROOM_DB_NAME).getCollection(MESSAGES_COLLECTION_NAME).insertOne(new Document()
-				.append("timestamp", timestamp.getTime()).append("user", user).append("message", message));
-	}
+    private void addTestMessageToDatabase(Timestamp timestamp, String user, String message) {
+        mongoClient.getDatabase(CHATROOM_DB_NAME).getCollection(MESSAGES_COLLECTION_NAME).insertOne(new Document()
+                .append("timestamp", timestamp.getTime()).append("user", user).append("message", message));
+    }
 
 }
